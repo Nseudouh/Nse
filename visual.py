824fa561c0cf9ed3716fa32b2155bf1cdb6ba943
Nseudouh <5udoun19@solent.ac.uk>
2021-12-29 15:07:19 +0000

    Created a function to display the top_5_countries

--- visual.py
+++ visual.py
@@ -16,3 +16,21 @@ the data using Matplotlib.
 """
 
 # TODO: Your code here
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import database
+import matplotlib.animation as animation
+
+def display_top_5_countries_confirmed():
+    country_names = database.get_top_5_countries_confirmed()
+    values = np.array([item[1] for item in country_names])
+    label = [item[0] for item in country_names]
+    # A function to compute values to be displayed on each wedge of the pie chart
+    def absolute_value(x):
+        x = np.round(x/100 * values.sum(), 0)
+        return x
+    plt.pie(values, labels = label, autopct= absolute_value)
+    plt.title('Top 5 countries for confirmed cases')
+    plt.show()
